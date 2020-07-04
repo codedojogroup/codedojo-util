5e7212ab4e67173b6aa1b80a16dc9a410ae223a6
Oscar Lodriguez (OSV) <oscar.lodriguez.osv@fedex.com>
2020-07-04 08:40:42 +0200

    removed old mock server

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-import * as handlers from './src/models/handler';
 import * as question from './src/models/question';
-import * as routes from './src/models/routes';
 import * as instructor from './src/models/instructor';
 
-export { handlers, question, routes, instructor }
+export {  question, instructor }
